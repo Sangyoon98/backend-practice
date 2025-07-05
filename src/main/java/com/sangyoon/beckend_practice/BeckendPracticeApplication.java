9583d2408905e7e774c6fb86efcf6bb3eecea3fb
Sangyoon98 <qlrqod123123@naver.com>
2025-07-05 22:45:54 +0900

    inital commit

---
+++ src/main/java/com/sangyoon/beckend_practice/BeckendPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sangyoon.beckend_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeckendPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeckendPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangyoon/beckend_practice/BeckendPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangyoon.beckend_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeckendPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
